17de729831150e7e3e41d1ea5ffab0bc5322a980
adityaRaj72 <87148756+adityaRaj72@users.noreply.github.com>
2026-04-25 02:04:55 +0530

    Change Vite server host to mockmate-iy6p.onrender.com
    fixed the bug where render could not show website cuz host url was not allowed in vite cors

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   server: {
-    host: "::",
+    host: "mockmate-iy6p.onrender.com",
     port: 8080,
     hmr: {
       overlay: false,
