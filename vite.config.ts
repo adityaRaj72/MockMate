ca5f0923c190cd8cc4379a94a88e7875b7f25f01
adityaRaj72 <87148756+adityaRaj72@users.noreply.github.com>
2026-04-25 02:12:09 +0530

    Update Vite server host configuration
    fixed the bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   server: {
-    host: "mockmate-iy6p.onrender.com",
+    host: "::",
+    allowedHosts: ["mockmate-iy6p.onrender.com"],
     port: 8080,
     hmr: {
       overlay: false,
